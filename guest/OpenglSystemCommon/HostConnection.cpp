232c785e771653d8a4554ed64aec0b81daee1c8d
Jason Macnak <natsu@google.com>
2023-07-19 14:45:43 -0700

    Make VirtGpu* interfaces
    ... so that upcoming end2end tests can set up a mock impl that
    interacts with the host within-process.
    
    Bug: b/292257025
    Test: m && cvd start
    Change-Id: I9d1fbaf3c2e50831a03405ad4f37b477b224abd9

--- guest/OpenglSystemCommon/HostConnection.cpp
+++ guest/OpenglSystemCommon/HostConnection.cpp
@@ -347,9 +347,8 @@ std::unique_ptr<HostConnection> HostConnection::connect(uint32_t capset_id) {
             break;
         }
         case HOST_CONNECTION_VIRTIO_GPU_ADDRESS_SPACE: {
-            VirtGpuDevice& instance =
-                VirtGpuDevice::getInstance((enum VirtGpuCapset)kCapsetGfxStreamVulkan);
-            auto deviceHandle = instance.getDeviceHandle();
+            auto device = VirtGpuDevice::getInstance((enum VirtGpuCapset)kCapsetGfxStreamVulkan);
+            auto deviceHandle = device->getDeviceHandle();
             auto stream = createVirtioGpuAddressSpaceStream(getGlobalHealthMonitor());
             if (!stream) {
                 ALOGE("Failed to create virtgpu AddressSpaceStream\n");
