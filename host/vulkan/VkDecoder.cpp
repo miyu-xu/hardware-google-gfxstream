e4bc0f1cdb496473dba9c8e1d07fba76a3c77138
Jason Macnak <natsu@google.com>
2023-07-20 13:40:09 -0700

    Update namespace in libandroidemu to gfxstream::guest
    ... to avoid conflicts when the guest is built for the host
    for end2end tests.
    
    In the future, the differences between libandroidemu and
    hardware/google/aemu/base/ should be consolidated and there
    should be a single shared library as there isn't really
    anything guest/host specific about either of these copies.
    
    Bug: b/292257025
    Test: cvd start --gpu_mode=gfxstream
          cvd start --gpu_mode=gfxstream_guest_angle_host_swiftshader
    Change-Id: I8bc5589958ee66a603cbfbc22ca0b563cee98f99

--- host/vulkan/VkDecoder.cpp
+++ host/vulkan/VkDecoder.cpp
@@ -163,8 +163,7 @@ size_t VkDecoder::Impl::decode(void* buf, size_t len, IOStream* ioStream,
             }
         }
 
-        std::atomic<uint32_t>* seqnoPtr = processResources ?
-                processResources->getSequenceNumberPtr() : nullptr;
+        std::atomic<uint32_t>* seqnoPtr = processResources->getSequenceNumberPtr();
 
         if (queueSubmitWithCommandsEnabled &&
             ((opcode >= OP_vkFirst && opcode < OP_vkLast) ||
