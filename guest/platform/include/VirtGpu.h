58b3e5d63f4c9e682c3e36699707a3d8d252d7ce
Jason Macnak <natsu@google.com>
2023-07-21 14:57:45 -0700

    Update VirtioGpuPipeStream to use VirtGpu abstraction
    Bug: b/292257025
    Test: GfxstreamEnd2EndTests
    Change-Id: I4f33a25fb23a10b1e93d39ba6fbf60a6132e32a4

--- guest/platform/include/VirtGpu.h
+++ guest/platform/include/VirtGpu.h
@@ -124,6 +124,9 @@ class VirtGpuBlob {
 
     virtual VirtGpuBlobMappingPtr createMapping(void) = 0;
     virtual int exportBlob(struct VirtGpuExternalHandle& handle) = 0;
+
+    virtual int transferFromHost(uint32_t offset, uint32_t size) = 0;
+    virtual int transferToHost(uint32_t offset, uint32_t size) = 0;
 };
 
 class VirtGpuBlobMapping {
