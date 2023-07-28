26220ad2d22f7ed49efa504003ec854e15dc91d2
Jason Macnak <natsu@google.com>
2023-07-28 08:52:02 -0700

    Add Vulkan-Hpp generated from Gfxstream's vk.xml
    ... which includes VK_ANDROID_native_buffer.
    
    Bug: b/292257025
    Test: atest --host GfxstreamEnd2EndTests
    Change-Id: If07eab38e275876f4a61bf2189fb43e9ed49ef6f

---
+++ codegen/vulkan/vulkan-hpp/generated/vulkan_format_traits.hpp
@@ -0,0 +1,17 @@
+// Copyright 2015-2021 The Khronos Group Inc.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+
+// This header is generated from the Khronos Vulkan XML API Registry.
+
+#ifndef VULKAN_FORMAT_TRAITS_HPP
+#define VULKAN_FORMAT_TRAITS_HPP
+
+#include <vulkan/vulkan.hpp>
+
+namespace VULKAN_HPP_NAMESPACE
+{
+
+}  // namespace VULKAN_HPP_NAMESPACE
+#endif
