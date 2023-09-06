1dc8df4ce39568b34c90a7658d8c0ab46149b904
Gurchetan Singh <gurchetansingh@google.com>
2023-09-06 16:56:28 -0700

    gfxstream: refactor process pipe logic
    - Nuke GoldfishProcessPipe + ProcessPipe classes
    - Nuke `sProcPipeOnce`
    - Nuke processPipeRestart
    - Nuke some Fuchsia process pipe code: it should
      just use virtio-gpu and deprecate goldfish pipe
      interfaces
    
    - Call processPipeInit(..) at the end of
      HostConnection::connect(..)
    - Call rcEnc->rcSetPuid(..) at the end
      HostConnection::rcEncoder()
    
    BUG=b:297245651
    TEST=compile
    
    Change-Id: I14e086d21de6bd4e1eb54cd65753e93b537a4ce7

--- guest/OpenglSystemCommon/ProcessPipe.h
+++ guest/OpenglSystemCommon/ProcessPipe.h
@@ -33,12 +33,11 @@
 
 struct renderControl_encoder_context_t;
 
-extern bool processPipeInit(int stream_handle, HostConnectionType connType, renderControl_encoder_context_t *rcEnc);
+extern bool processPipeInit(int stream_handle, HostConnectionType connType);
 extern uint64_t getPuid();
 
-// For testing purposes; this will close the current process pipe if opened, reset the state to initial,
-// and open it again with the same parameters.
-extern void processPipeRestart();
+// For testing purposes; this will close the current process pipe if opened, reset the state to
+// initial, and open it again with the same parameters.
 extern void refreshHostConnection();
 extern void disableProcessPipeForTesting();
 
