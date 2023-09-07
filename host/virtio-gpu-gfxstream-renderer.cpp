61776a254ead4aea63d899cc9c95886920108cd7
Gurchetan Singh <gurchetansingh@google.com>
2023-09-07 10:17:45 -0700

    gfxstream: advertise noRenderControlEnc
    Enable the feature.
    
    BUG=297245651
    TEST=compile
    
    Change-Id: I29ae8e3469ba9249f7d343cce368f202384140db

--- host/virtio-gpu-gfxstream-renderer.cpp
+++ host/virtio-gpu-gfxstream-renderer.cpp
@@ -1387,6 +1387,7 @@ class PipeVirglRenderer {
                         *vk_emu->representativeColorBufferMemoryTypeIndex;
                 }
 
+                capset->noRenderControlEnc = 1;
                 capset->blobAlignment = mPageSize;
                 if (vk_emu && vk_emu->live) {
                     capset->deferredMapping = 1;
