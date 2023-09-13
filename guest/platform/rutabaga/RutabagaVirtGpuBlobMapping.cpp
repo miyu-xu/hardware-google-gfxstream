96290ed1b56236ac2eb9969fc8911c15d49215cd
Jason Macnak <natsu@google.com>
2023-09-13 08:27:41 -0700

    Promote testing framework to a proper platform backend
    ... so that a Gfxstream VK ICD can be built which runs
    the host server in process.
    
    Bug: b/293897204
    Test: GfxstreamEnd2EndTests
    Change-Id: I148d2703744f4bd3e6bf5f111377134abfb5e618

--- guest/platform/rutabaga/RutabagaVirtGpuBlobMapping.cpp
+++ guest/platform/rutabaga/RutabagaVirtGpuBlobMapping.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,28 +14,20 @@
  * limitations under the License.
  */
 
- #include "VirtGpu.h"
+#include "RutabagaVirtGpu.h"
 
-namespace {
+#include "RutabagaLayer.h"
 
-static VirtGpuDevice* sDevice = nullptr;
+namespace gfxstream {
 
-}  // namespace
+RutabagaVirtGpuBlobMapping::RutabagaVirtGpuBlobMapping(VirtGpuBlobPtr blob, uint8_t* mapped)
+    : mBlob(blob),
+      mMapped(mapped) {}
 
-VirtGpuDevice* VirtGpuDevice::getInstance(enum VirtGpuCapset capset) {
-    if (!sDevice) {
-        sDevice = createPlatformVirtGpuDevice(capset);
-    }
-    return sDevice;
+RutabagaVirtGpuBlobMapping::~RutabagaVirtGpuBlobMapping(void) {
+    EmulatedVirtioGpu::Get().Unmap(mBlob->getResourceHandle());
 }
 
-void VirtGpuDevice::resetInstance() {
-    if (sDevice) {
-        delete sDevice;
-        sDevice = nullptr;
-    }
-}
+uint8_t* RutabagaVirtGpuBlobMapping::asRawPtr(void) { return mMapped; }
 
-void VirtGpuDevice::setInstanceForTesting(VirtGpuDevice* device) {
-    sDevice = device;
-}
+}  // namespace gfxstream
--- guest/platform/rutabaga/include/gfxstream/RutabagaLayerTestUtils.h
+++ guest/platform/rutabaga/include/gfxstream/RutabagaLayerTestUtils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,28 +14,10 @@
  * limitations under the License.
  */
 
- #include "VirtGpu.h"
+#pragma once
 
-namespace {
+namespace gfxstream {
 
-static VirtGpuDevice* sDevice = nullptr;
+void ResetEmulatedVirtioGpu();
 
-}  // namespace
-
-VirtGpuDevice* VirtGpuDevice::getInstance(enum VirtGpuCapset capset) {
-    if (!sDevice) {
-        sDevice = createPlatformVirtGpuDevice(capset);
-    }
-    return sDevice;
-}
-
-void VirtGpuDevice::resetInstance() {
-    if (sDevice) {
-        delete sDevice;
-        sDevice = nullptr;
-    }
-}
-
-void VirtGpuDevice::setInstanceForTesting(VirtGpuDevice* device) {
-    sDevice = device;
-}
+}  // namespace gfxstream
