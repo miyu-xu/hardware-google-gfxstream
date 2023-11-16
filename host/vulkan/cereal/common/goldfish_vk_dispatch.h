6f9acbb5d2df884bceeedd2322244a9ad7f5e636
Aaron Ruby <aruby@blackberry.com>
2023-11-16 12:21:13 -0500

    gfxstream: cereal: Add VK_EXT_depth_clip_enable as supported feature.
    It's needed by some Linux apps.
    
    BUG=298679844
    TEST=compile
    
    Change-Id: I9f8d56b9192a903ebfbfb98ea8bc51183555f6aa

--- host/vulkan/cereal/common/goldfish_vk_dispatch.h
+++ host/vulkan/cereal/common/goldfish_vk_dispatch.h
@@ -488,6 +488,8 @@ struct VulkanDispatch {
 #endif
 #ifdef VK_EXT_texture_compression_astc_hdr
 #endif
+#ifdef VK_EXT_depth_clip_enable
+#endif
 #ifdef VK_EXT_swapchain_colorspace
 #endif
 #ifdef VK_EXT_queue_family_foreign
