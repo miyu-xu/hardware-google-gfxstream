126017ced1990744eb2fef812b7afd8b79a7c246
Yahan Zhou <yahan@google.com>
2023-11-16 15:53:16 -0800

    Add basic Vulkan snapshot tests for images
    This test simply creates an image and performs a snapshot.
    
    Bug: 294277842
    Test: GfxstreamEnd2EndVkSnapshotImageTest
    Change-Id: If0e0df965be79b5ce0c918565d243e3c5a215f04

--- guest/vulkan_enc/HostVisibleMemoryVirtualization.cpp
+++ guest/vulkan_enc/HostVisibleMemoryVirtualization.cpp
@@ -55,6 +55,16 @@ CoherentMemory::~CoherentMemory() {
                                                                      false);
 }
 
+void CoherentMemory::updateMapping(VirtGpuBlobMappingPtr mapping) {
+    mBlobMapping = mapping;
+    if (mapping) {
+        mAllocator =
+            std::make_unique<gfxstream::guest::SubAllocator>(mapping->asRawPtr(), mSize, 4096);
+    } else {
+        mAllocator.release();
+    }
+}
+
 VkDeviceMemory CoherentMemory::getDeviceMemory() const { return mMemory; }
 
 bool CoherentMemory::subAllocate(uint64_t size, uint8_t** ptr, uint64_t& offset) {
@@ -70,6 +80,10 @@ bool CoherentMemory::release(uint8_t* ptr) {
     mAllocator->free(ptr);
     return true;
 }
+
+bool CoherentMemory::valid() { return mBlobMapping && mBlobMapping->valid(); }
+
+VirtGpuBlobMappingPtr CoherentMemory::getMapping() { return mBlobMapping; }
 
 }  // namespace vk
 }  // namespace gfxstream
