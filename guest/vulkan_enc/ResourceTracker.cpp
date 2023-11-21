4cdf59934a9f9d861bc9b6471ada0d729430dfad
Gurchetan Singh <gurchetansingh@google.com>
2023-11-21 12:33:08 -0800

    gfxstream: ResourceTracker: don't crash without VkBindImageMemorySwapchainInfoKHR
    Not necessary to crash in this case.
    
    BUG=313466265
    TEST=compile
    
    Change-Id: I3fcf3ee06912a31cd2313da1db0dbd7b58b75096

--- guest/vulkan_enc/ResourceTracker.cpp
+++ guest/vulkan_enc/ResourceTracker.cpp
@@ -5911,8 +5911,7 @@ void ResourceTracker::unwrap_VkBindImageMemorySwapchainInfoKHR(
     }
 
     if (!outputBimsi || !outputBimsi->swapchain) {
-        ALOGE("FATAL: Local VkBindImageMemorySwapchainInfoKHR not properly allocated!");
-        abort();
+        return;
     }
 
     // Android based swapchains are implemented by the Android framework's
