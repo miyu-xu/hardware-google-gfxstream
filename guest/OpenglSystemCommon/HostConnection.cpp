a257318bd623191058dd8288efbf82f0c43d7960
Gurchetan Singh <gurchetansingh@google.com>
2024-01-19 17:57:15 -0800

    gfxstream: nuke goldfish_dma
    Goldfish DMA no longer has any kernel support, and that
    code path looks unused anyways.
    
    https://android-review.googlesource.com/c/kernel/common-modules/virtual-device/+/2919253
    
    BUG=311486792
    TEST=compile
    
    Change-Id: I9860086fc8015ab1be0fb9017a47d477509b9b09

--- guest/OpenglSystemCommon/HostConnection.cpp
+++ guest/OpenglSystemCommon/HostConnection.cpp
@@ -491,7 +491,6 @@ ExtendedRCEncoderContext *HostConnection::rcEncoder()
         ExtendedRCEncoderContext* rcEnc = m_rcEnc.get();
         setChecksumHelper(rcEnc);
         queryAndSetSyncImpl(rcEnc);
-        queryAndSetDmaImpl(rcEnc);
         queryAndSetGLESMaxVersion(rcEnc);
         queryAndSetNoErrorState(rcEnc);
         queryAndSetHostCompositionImpl(rcEnc);
@@ -615,15 +614,6 @@ void HostConnection::queryAndSetSyncImpl(ExtendedRCEncoderContext *rcEnc) {
         rcEnc->setSyncImpl(SYNC_IMPL_NATIVE_SYNC_V2);
     } else {
         rcEnc->setSyncImpl(SYNC_IMPL_NONE);
-    }
-}
-
-void HostConnection::queryAndSetDmaImpl(ExtendedRCEncoderContext *rcEnc) {
-    std::string hostExtensions = queryHostExtensions(rcEnc);
-    if (hostExtensions.find(kDmaExtStr_v1) != std::string::npos) {
-        rcEnc->setDmaImpl(DMA_IMPL_v1);
-    } else {
-        rcEnc->setDmaImpl(DMA_IMPL_NONE);
     }
 }
 
