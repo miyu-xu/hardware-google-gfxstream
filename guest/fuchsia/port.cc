4df12494304f20461f97fc8c58339b3d36b3089e
C Stout <cstout@google.com>
2024-01-31 17:28:40 -0800

    [guest] Remove Fuchsia syslog_static dependency
    Change-Id: I4ca6239b3386f923925637ed0b279abbc2a5d37e

--- guest/fuchsia/port.cc
+++ guest/fuchsia/port.cc
@@ -13,7 +13,8 @@
 #include <cstring>
 
 #if defined(__Fuchsia__)
-#include <lib/syslog/global.h>
+#include <lib/syslog/structured_backend/cpp/fuchsia_syslog.h>
+#include <log/log.h>
 #else
 #include <libgen.h>
 #endif
@@ -45,59 +46,59 @@ int property_get(const char* key, char* value, const char* default_value) {
   return 0;
 }
 
-int __android_log_print(int priority, const char* tag, const char* file,
-                        int line, const char* format, ...) {
-  const char* local_tag = tag;
-  if (!local_tag) {
-    local_tag = "<NO_TAG>";
-  }
+int __android_log_print(int priority, const char* tag, const char* file, int line,
+                        const char* format, ...) {
+    const char* local_tag = tag;
+    if (!local_tag) {
+        local_tag = "<NO_TAG>";
+    }
 
-  va_list ap;
-  va_start(ap, format);
+    va_list ap;
+    va_start(ap, format);
 #if defined(__Fuchsia__)
-  switch (priority) {
-    case ANDROID_LOG_VERBOSE:
-    case ANDROID_LOG_DEBUG:
-      FX_LOGVF(DEBUG, local_tag, file, line, format, ap);
-      break;
-    case ANDROID_LOG_WARN:
-      FX_LOGVF(WARNING, local_tag, file, line, format, ap);
-      break;
-    case ANDROID_LOG_ERROR:
-      FX_LOGVF(ERROR, local_tag, file, line, format, ap);
-      break;
-    case ANDROID_LOG_FATAL:
-      FX_LOGVF(FATAL, local_tag, file, line, format, ap);
-      break;
-    case ANDROID_LOG_INFO:
-    default:
-      FX_LOGVF(INFO, local_tag, file, line, format, ap);
-      break;
-  }
+    switch (priority) {
+        case ANDROID_LOG_VERBOSE:
+        case ANDROID_LOG_DEBUG:
+            gfxstream_fuchsia_log(FUCHSIA_LOG_DEBUG, local_tag, file, line, format, ap);
+            break;
+        case ANDROID_LOG_WARN:
+            gfxstream_fuchsia_log(FUCHSIA_LOG_WARNING, local_tag, file, line, format, ap);
+            break;
+        case ANDROID_LOG_ERROR:
+            gfxstream_fuchsia_log(FUCHSIA_LOG_ERROR, local_tag, file, line, format, ap);
+            break;
+        case ANDROID_LOG_FATAL:
+            gfxstream_fuchsia_log(FUCHSIA_LOG_FATAL, local_tag, file, line, format, ap);
+            break;
+        case ANDROID_LOG_INFO:
+        default:
+            gfxstream_fuchsia_log(FUCHSIA_LOG_INFO, local_tag, file, line, format, ap);
+            break;
+    }
 #else
-  linux_log_prefix(local_tag, file, line, format, ap);
+    linux_log_prefix(local_tag, file, line, format, ap);
 #endif
 
-  return 1;
+    return 1;
 }
 
-void __android_log_assert(const char* condition, const char* tag,
-                          const char* file, int line, const char* format, ...) {
-  const char* local_tag = tag;
-  if (!local_tag) {
-    local_tag = "<NO_TAG>";
-  }
-  va_list ap;
-  va_start(ap, format);
+void __android_log_assert(const char* condition, const char* tag, const char* file, int line,
+                          const char* format, ...) {
+    const char* local_tag = tag;
+    if (!local_tag) {
+        local_tag = "<NO_TAG>";
+    }
+    va_list ap;
+    va_start(ap, format);
 #if defined(__Fuchsia__)
-  FX_LOGVF(ERROR, local_tag, file, line, format, ap);
+    gfxstream_fuchsia_log(FUCHSIA_LOG_ERROR, local_tag, file, line, format, ap);
 #else
-  linux_log_prefix(local_tag, file, line, format, ap);
+    linux_log_prefix(local_tag, file, line, format, ap);
 #endif
 
-  va_end(ap);
+    va_end(ap);
 
-  abort();
+    abort();
 }
 
 int sync_wait(int fd, int timeout) { return -1; }
