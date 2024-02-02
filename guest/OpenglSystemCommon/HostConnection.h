2e548ba4b46f927b524facb9d66b28d2f6267d32
Yahan Zhou <yahan@google.com>
2024-02-02 20:17:01 +0000

    Revert "gfxstream: always initialize rcEncoder and other things"
    This reverts commit 39077a8092e24ba639007b4f5300365826121a7c.
    
    Reason for revert: potential culprit for b/323552528
    
    Change-Id: I74c91a01ff0cad1687b14492e2673e5de082fbc0

--- guest/OpenglSystemCommon/HostConnection.h
+++ guest/OpenglSystemCommon/HostConnection.h
@@ -140,6 +140,7 @@ class HostConnection
 {
 public:
     static HostConnection *get();
+    static bool isInit();
     static HostConnection* getOrCreate(enum VirtGpuCapset capset = kCapsetNone);
 
     static HostConnection* getWithThreadInfo(EGLThreadInfo* tInfo,
