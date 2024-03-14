0fcbed05c6e3091b96eaf85a4756b08fb06600fb
Jason Macnak <natsu@google.com>
2024-03-14 10:21:07 -0700

    Add test triggering the deadlock seen in b/329287602
    Background: RenderThreads may go to sleep if they go through
    kMaxUnavailableReads cycles of not receiving any commands from the guest.
    After going asleep, the RenderThread must be woken up via a WAKEUP command
    which currently comes from GFXSTREAM_CONTEXT_PING commands over virtio gpu's
    VIRTIO_GPU_CMD_SUBMIT_3D.
    
    ASG virtio gpu resource clean up would wait for the underlying RenderThread
    using that resource clean up to finish
    (`RendererImpl::addressSpaceGraphicsConsumerDestroy()` calling
    `thread->wait();`). This could result in deadlocks if
    
    * The main virtio gpu command thread is waiting for ASG-RenderThread-1 to
      finish as a part of resource destruction via a virtio gpu
      VIRTIO_GPU_CMD_CTX_DETACH_RESOURCE command.
    
    and
    
    * ASG-RenderThread-1 is asleep waiting for a WAKEUP via a
      VIRTIO_GPU_CMD_SUBMIT_3D command from the main virtio gpu thread.
    
    (This can be further complicated when RenderThreads are dependent on each
    other. Multi-threaded Vulkan apps rely on a sequence number to ensure that
    commands are executed in order on the host. Gfxstream's Vulkan decoders will
    spin loop waiting for their turn to process their next command. With this,
    RenderThreads can end up in dependency chains where Thread-1 is waiting to
    process the command with sequence number N is dependent on Thread-2
    processing the command with sequence number N - 1, Thread-2 is waiting on
    Thread-3, etc.)
    
    Bug: b/329287602
    Test: newly added test hangs
    Change-Id: Ia54f3b49827d3de908e787b4508163fddc6f1d14

--- common/end2end/GfxstreamEnd2EndVkTests.cpp
+++ common/end2end/GfxstreamEnd2EndVkTests.cpp
@@ -446,6 +446,47 @@ TEST_P(GfxstreamEnd2EndVkTest, DISABLED_DescriptorSetAllocFreeDestroy) {
     EXPECT_THAT(device->freeDescriptorSets(*bundle.descriptorPool, kNumSets, descriptorSetHandles.data()), IsVkSuccess());
 }
 
+TEST_P(GfxstreamEnd2EndVkTest, DISABLED_MultiThreadedShutdown) {
+    constexpr const int kNumIterations = 100;
+    for (int i = 0; i < kNumIterations; i++) {
+        auto [instance, physicalDevice, device, queue, queueFamilyIndex] =
+                VK_ASSERT(SetUpTypicalVkTestEnvironment());
+
+        const vkhpp::BufferCreateInfo bufferCreateInfo = {
+            .size = 1024,
+            .usage = vkhpp::BufferUsageFlagBits::eTransferSrc,
+        };
+
+        std::atomic_int threadsReady{0};
+        std::vector<std::thread> threads;
+
+        constexpr const int kNumThreads = 5;
+        for (int threadIndex = 0; threadIndex < kNumThreads; threadIndex++) {
+            threads.emplace_back([&, this, threadIndex](){
+                // Perform some work to ensure host RenderThread started.
+                auto buffer1 = device->createBufferUnique(bufferCreateInfo).value;
+
+                ++threadsReady;
+                while (threadsReady.load() != kNumThreads) {}
+
+                // Sleep a little which is hopefully enough time to potentially get
+                // the corresponding host ASG RenderThreads to go sleep waiting for
+                // a WAKEUP via a GFXSTREAM_CONTEXT_PING.
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+                // Perform a little more work which could potentially cause some
+                // interleaving between the threads sending GFXSTREAM_CONTEXT_PING
+                // commands and the threads shutting down their ASGs.
+                auto buffer2 = device->createBufferUnique(bufferCreateInfo).value;
+            });
+        }
+
+        for (auto& thread : threads) {
+            thread.join();
+        }
+    }
+}
+
 INSTANTIATE_TEST_CASE_P(GfxstreamEnd2EndTests, GfxstreamEnd2EndVkTest,
                         ::testing::ValuesIn({
                             TestParams{
@@ -456,7 +497,6 @@ INSTANTIATE_TEST_CASE_P(GfxstreamEnd2EndTests, GfxstreamEnd2EndVkTest,
                             TestParams{
                                 .with_gl = true,
                                 .with_vk = true,
-                                .with_transport = GfxstreamTransport::kVirtioGpuAsg,
                             },
                             TestParams{
                                 .with_gl = false,
