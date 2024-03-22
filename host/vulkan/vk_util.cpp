ae141e4270d616c0351fa55793dde01b6be83276
Bo Hu <bohu@google.com>
2024-03-22 13:55:02 -0700

    detect vk version in gfxstream
    wip
    
    Change-Id: I986cd0f3a76a14c8bbdb5a61ec7cac7aa0280ea5

--- host/vulkan/vk_util.cpp
+++ host/vulkan/vk_util.cpp
@@ -15,6 +15,8 @@
  */
 
 #include "vk_util.h"
+#include "VulkanDispatch.h"
+#include "host-common/opengl/misc.h"
 
 namespace gfxstream {
 namespace vk {
@@ -31,6 +33,41 @@ void setVkCheckCallbacks(std::unique_ptr<VkCheckCallbacks> callbacks) {
 }
 
 const CallbacksWrapper<VkCheckCallbacks>& getVkCheckCallbacks() { return *gVkCheckCallbacks; }
+
+bool detectVulkanDriverApiVersion() {
+    VulkanDispatch* vk = vkDispatch(true);
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Graphics Engine";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+    appInfo.pEngineName = "fun engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+    appInfo.apiVersion = VK_API_VERSION_1_3;
+
+    VkInstanceCreateInfo createInfo {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t extensionCount = 1;
+    const char** extensions = (const char**)(malloc(sizeof(const char*) * 1));
+    extensions[0] = "VK_KHR_surface";
+    createInfo.enabledExtensionCount = extensionCount;
+    createInfo.ppEnabledExtensionNames = extensions;
+
+    VkInstance instance;
+
+    VkResult result = vk->vkCreateInstance(&createInfo, 0, &instance);
+
+    if (result != VK_SUCCESS) {
+        fprintf(stderr, "failed to create instance %d\n", result);
+        return false;
+    } else {
+        fprintf(stderr, "successfully created instance %d\n", result);
+    }
+
+    emugl::setVulkanVersion(1, 3, 0);
+    return true;
+}
 
 std::optional<uint32_t> findMemoryType(const VulkanDispatch* ivk, VkPhysicalDevice physicalDevice,
                                        uint32_t typeFilter, VkMemoryPropertyFlags properties) {
