8236e326f9eadecd2ab4858f18ddc3ee058446fe
Yahan Zhou <yahan@google.com>
2024-06-13 15:59:14 -0700

    Add snapshot support for vkBindImageMemory2
    The pattern of vkBindImageMemory2 doesn't really fit with what we have
    in the auto-code generator. Thus we need to implement a special entry
    for it in the code generator. In future we might want to split the code
    and directly write c++ code for similar APIs.
    
    Note that due to complexity we only support vkBindImageMemory2 with
    bindInfoCount == 1, for now.
    
    Bug: 346573423
    Test: ImageViewDependency2
    Change-Id: Ib739391c1fe2073725df68380ed832a05e8070b1

--- host/vulkan/VkDecoderGlobalState.cpp
+++ host/vulkan/VkDecoderGlobalState.cpp
@@ -2327,6 +2327,14 @@ class VkDecoderGlobalState::Impl {
     VkResult on_vkBindImageMemory2(android::base::BumpPool* pool, VkDevice boxed_device,
                                    uint32_t bindInfoCount,
                                    const VkBindImageMemoryInfo* pBindInfos) {
+        if (bindInfoCount > 1 && snapshotsEnabled()) {
+            GFXSTREAM_LOG(
+                stderr, 'I',
+                "vkBindImageMemory2 with more than 1 bindInfoCount not supporting snapshot");
+            get_emugl_vm_operations().setSkipSnapshotSave(true);
+            get_emugl_vm_operations().setSkipSnapshotSaveReason(SNAPSHOT_SKIP_UNSUPPORTED_VK_API);
+        }
+
         auto device = unbox_VkDevice(boxed_device);
         auto vk = dispatch_VkDevice(boxed_device);
         bool needEmulation = false;
