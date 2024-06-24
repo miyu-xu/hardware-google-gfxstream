05c8086fec550abe5447945cae4e71bd28b4ba13
Serdar Kocdemir <kocdemir@google.com>
2024-06-24 10:22:02 +0100

    Define metal objects extension structures
    Prepare structure definitions to be used for external memory
    operations over the metal objects extension.
    Also includes some trivial comment and error message changes.
    
    Test: build and run with -use-host-vulkan
    Change-Id: I4b69a298039c8b66a8b93a1ee1fa73d233131d4a

--- host/vulkan/VkDecoderGlobalState.cpp
+++ host/vulkan/VkDecoderGlobalState.cpp
@@ -6774,7 +6774,7 @@ class VkDecoderGlobalState::Impl {
     void set_boxed_non_dispatchable_##type(type boxed, type underlying) {                         \
         DispatchableHandleInfo<uint64_t> item;                                                    \
         item.underlying = (uint64_t)underlying;                                                   \
-        sBoxedHandleManager.update((uint64_t)boxed, item, Tag_##type);                          \
+        sBoxedHandleManager.update((uint64_t)boxed, item, Tag_##type);                            \
     }                                                                                             \
     type unboxed_to_boxed_non_dispatchable_##type(type unboxed) {                                 \
         AutoLock lock(sBoxedHandleManager.lock);                                                  \
@@ -6899,6 +6899,11 @@ class VkDecoderGlobalState::Impl {
             }
             if (hasDeviceExtension(properties, VK_EXT_METAL_OBJECTS_EXTENSION_NAME)) {
                 res.push_back(VK_EXT_METAL_OBJECTS_EXTENSION_NAME);
+            }
+        } else {
+            // Non-MoltenVK path, use memory_fd
+            if (hasDeviceExtension(properties, VK_KHR_EXTERNAL_MEMORY_FD_EXTENSION_NAME)) {
+                res.push_back(VK_KHR_EXTERNAL_MEMORY_FD_EXTENSION_NAME);
             }
         }
 #endif
