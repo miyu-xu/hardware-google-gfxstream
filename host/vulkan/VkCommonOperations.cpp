8ed1e2888371f0c424e73649a505252a92f7ab58
Gurchetan Singh <gurchetansingh@chromium.org>
2024-06-24 14:31:57 -0700

    gfxstream: host: specify stream handle type when exporting VkBuffer
    This is useful information to know.
    
    BUG=349122558
    TEST=compile
    
    Change-Id: I89f594b9d6c2ab716f094b66659b9440eba0c2ee

--- host/vulkan/VkCommonOperations.cpp
+++ host/vulkan/VkCommonOperations.cpp
@@ -3334,7 +3334,8 @@ bool teardownVkBuffer(uint32_t bufferHandle) {
     return true;
 }
 
-VK_EXT_MEMORY_HANDLE getBufferExtMemoryHandle(uint32_t bufferHandle) {
+VK_EXT_MEMORY_HANDLE getBufferExtMemoryHandle(uint32_t bufferHandle,
+                                              uint32_t* outStreamHandleType) {
     if (!sVkEmulation || !sVkEmulation->live) return VK_EXT_MEMORY_HANDLE_INVALID;
 
     AutoLock lock(sVkEmulationLock);
@@ -3345,6 +3346,7 @@ VK_EXT_MEMORY_HANDLE getBufferExtMemoryHandle(uint32_t bufferHandle) {
         return VK_EXT_MEMORY_HANDLE_INVALID;
     }
 
+    *outStreamHandleType = infoPtr->memory.streamHandleType;
     return infoPtr->memory.externalHandle;
 }
 
