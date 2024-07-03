9ea7f38f60fe4ee0591686bfc16c16e18d7afab6
Gurchetan Singh <gurchetansingh@chromium.org>
2024-07-03 10:27:40 -0700

    gfxstream: guest: add GFXSTREAM_CREATE_SYNC protocol
    Guest side needs to check availability of new protocol.
    
    BUG=350965057
    TEST=compile
    
    Change-Id: Id26c246555376001450d72b051af43b4c7ead6ef

--- host/virtio-gpu-gfxstream-renderer.cpp
+++ host/virtio-gpu-gfxstream-renderer.cpp
@@ -1630,6 +1630,10 @@ class PipeVirglRenderer {
 #if SUPPORT_DMABUF
                 capset->alwaysBlob = 1;
 #endif
+
+#if GFXSTREAM_UNSTABLE_VULKAN_EXTERNAL_SYNC
+                capset->externalSync = 1;
+#endif
                 break;
             }
             case VIRTGPU_CAPSET_GFXSTREAM_MAGMA: {
