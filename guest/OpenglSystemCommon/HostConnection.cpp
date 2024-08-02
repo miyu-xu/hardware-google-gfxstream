89ef019dfacc0222529ec9e080788ab2fa96c026
Gurchetan Singh <gurchetansingh@google.com>
2024-08-02 14:03:13 -0700

    HACK TEST: gfxstream: guest: plumb the descriptor everywhere
    This change does two things:
        - Makes sure the descriptor is passed through to
          EmulatedGralloc, where it makes a difference
    
        - Makes sure descriptor is always used when creating
          a VirtioGpuPipeStream.  The VirtioGpuPipeStream
          API where the descriptor is not passed in has
          been nuked.
    
    GLES, VK, Gralloc, and RenderControl can all pass in
    a descriptor via their APIs with this change.
    
    BUG=354706346
    TEST=no regressions via End2EndTests
    
    Change-Id: Ibc5f6be95b04b267eaa2f671f32978898de52f1a

--- guest/OpenglSystemCommon/HostConnection.cpp
+++ guest/OpenglSystemCommon/HostConnection.cpp
@@ -209,7 +209,7 @@ std::unique_ptr<HostConnection> HostConnection::connect(enum VirtGpuCapset capse
         }
 #endif
         case HOST_CONNECTION_VIRTIO_GPU_PIPE: {
-            auto stream = new VirtioGpuPipeStream(STREAM_BUFFER_SIZE);
+            auto stream = new VirtioGpuPipeStream(STREAM_BUFFER_SIZE, -1);
             if (!stream) {
                 ALOGE("Failed to create VirtioGpu for host connection\n");
                 return nullptr;
@@ -242,7 +242,9 @@ std::unique_ptr<HostConnection> HostConnection::connect(enum VirtGpuCapset capse
     }
 
 #if defined(ANDROID)
-    con->m_grallocHelper.reset(gfxstream::createPlatformGralloc(con->m_rendernodeFd));
+    descriptor = (con->m_rendernodeFd >= 0) ? con->m_rendernodeFd : descriptor;
+    ALOGE("--- descriptor is %d ---", descriptor);
+    con->m_grallocHelper.reset(gfxstream::createPlatformGralloc(descriptor));
     if (!con->m_grallocHelper) {
         ALOGE("Failed to create platform Gralloc!");
         abort();
