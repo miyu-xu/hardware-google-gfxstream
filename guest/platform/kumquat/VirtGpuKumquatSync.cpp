7ebbcf693dd3f6afa6ab7cca51ba6187c145dd07
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-08-21 08:13:34 +0000

    Revert "gfxstream: simplify GLESv1/GLESv2/EGL/Vulkan builds"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/2a86c6e7-845a-4a0a-86c9-b8dc19012f9f). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I9775a45eff6a0a5d179e3df60b9e583f240ca59a
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- guest/platform/kumquat/VirtGpuKumquatSync.cpp
+++ guest/platform/kumquat/VirtGpuKumquatSync.cpp
@@ -47,6 +47,6 @@ int VirtGpuKumquatSyncHelper::dup(int syncFd) { return ::dup(syncFd); }
 
 int VirtGpuKumquatSyncHelper::close(int syncFd) { return ::close(syncFd); }
 
-SyncHelper* kumquatCreateSyncHelper() { return new VirtGpuKumquatSyncHelper(); }
+SyncHelper* createPlatformSyncHelper() { return new VirtGpuKumquatSyncHelper(); }
 
 }  // namespace gfxstream
