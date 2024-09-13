311e555cdb9cd3cce7bfc5cf74eab240f0aadedb
Yahan Zhou <yahan@google.com>
2024-09-13 13:29:41 -0700

    Use the right wait object on queue submit
    Now it will use the GPU lock if there is no more operation after queue
    submit. If we need to do VK->GL buffer copying after submitting, we use
    the CPU lock.
    
    Bug: 366153044
    Test: GfxstreamEnd2EndTests
    Change-Id: I8d9c6b62c02941af45ae864b89a7d43bdbdd470a

--- host/vulkan/VkDecoderGlobalState.cpp
+++ host/vulkan/VkDecoderGlobalState.cpp
@@ -5675,6 +5675,7 @@ class VkDecoderGlobalState::Impl {
         VkDevice device = VK_NULL_HANDLE;
         Lock* ql;
         std::shared_ptr<std::promise<void>> isWaitablePromise;
+        std::shared_future<void> isWaitable;
         {
             std::lock_guard<std::recursive_mutex> lock(mLock);
 
@@ -5702,8 +5703,13 @@ class VkDecoderGlobalState::Impl {
                 auto* fenceInfo = android::base::find(mFenceInfo, fence);
                 if (fenceInfo) {
                     isWaitablePromise = fenceInfo->isWaitablePromise;
+                    isWaitable = fenceInfo->isWaitable;
                 }
             }
+        }
+        if (isWaitablePromise == nullptr) {
+            isWaitablePromise.reset(new std::promise<void>());
+            isWaitable = isWaitablePromise->get_future().share();
         }
 
         VkFence usedFence = fence;
@@ -5719,8 +5725,15 @@ class VkDecoderGlobalState::Impl {
                 // of this queueSubmit
                 usedFence = builder.CreateFenceForOp();
             }
-            queueCompletedWaitable = builder.OnQueueSubmittedWithFence(usedFence);
-
+            // If releasedColorBuffers is empty, the color buffers are good for use
+            // right after the GPU fence is signaled.
+            // Otherwise, it will perform VK->GL copy after GPU fence and we need
+            // to wait for it.
+            if (releasedColorBuffers.empty()) {
+                queueCompletedWaitable = builder.OnQueueSubmittedWithFence(usedFence);
+            } else {
+                queueCompletedWaitable = isWaitable;
+            }
             deviceInfo->deviceOpTracker->PollAndProcessGarbage();
         }
 
@@ -5809,7 +5822,7 @@ class VkDecoderGlobalState::Impl {
         } else {
             SyncThread::get()->triggerGeneral(
                 [flushColorBuffer = m_emu->callbacks.flushColorBuffer, releasedColorBuffers,
-                 usedFence, isWaitablePromise, vk, device]() {
+                 usedFence, isWaitablePromise = std::move(isWaitablePromise), vk, device]() {
                     vk->vkWaitForFences(device, 1, &usedFence, true,
                                         10ULL * 1000ULL * 1000ULL * 1000ULL);
                     for (HandleType cb : releasedColorBuffers) {
