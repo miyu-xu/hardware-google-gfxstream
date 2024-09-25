3a7acf976071c90ba4461dd19017000e663fdd47
Serdar Kocdemir <kocdemir@google.com>
2024-09-25 13:49:39 +0100

    Add dimensions of colorbuffers to logging
    For an easier debugging of unnecessary color buffer creations.
    
    Bug: 368070298
    Test: build & presubmit
    Change-Id: I192ea48763c79f06c027facfb9f714f8edbb7b78

--- host/vulkan/VkCommonOperations.cpp
+++ host/vulkan/VkCommonOperations.cpp
@@ -2452,11 +2452,12 @@ bool initializeVkColorBufferLocked(
         getValidMemoryTypeIndex(infoPtr->memReqs.memoryTypeBits, infoPtr->memoryProperty);
 
     VERBOSE(
-        "ColorBuffer %d, "
+        "ColorBuffer %d, dimensions:%dx%d, "
         "allocation size and type index: %lu, %d, "
         "allocated memory property: %d, "
         "requested memory property: %d",
-        colorBufferHandle, infoPtr->memory.size, infoPtr->memory.typeIndex,
+        colorBufferHandle, infoPtr->width, infoPtr->height,
+        infoPtr->memory.size, infoPtr->memory.typeIndex,
         sVkEmulation->deviceInfo.memProps.memoryTypes[infoPtr->memory.typeIndex].propertyFlags,
         infoPtr->memoryProperty);
 
