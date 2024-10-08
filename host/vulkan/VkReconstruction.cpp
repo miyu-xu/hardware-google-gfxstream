27eebb50275b1864290f2d0b717a7346dc6c3912
Bo Hu <bohu@google.com>
2024-10-08 13:50:52 -0700

    [Vulkan Snapshot]: handle multi-allocation better
    Do not delete the ApiInfo that keeps the trace
    to create multiple Vulkan Objects, if it still
    has some objects left in its createdHandles.
    
    Only do that after all the handles are removed,
    so that the remaining handles will still be
    loaded and usable; Otherwise, the emulator will
    crash later on if one of the handles is used.
    
    This will potentiall lead to resource leakage,
    and it should be handled shortly.
    
    Test: create an vulkan app, allocate command
    buffers in one batch, e.g, 4, free the last
    command buffer; use the remaining 3 for rendering
    and save snapshot; it should not crash in save
    and it should load fine.
    
    Bug: 372242705
    
    Change-Id: If671e95acae9cf49e490d9e46ad393cddf6a3e49

--- host/vulkan/VkReconstruction.cpp
+++ host/vulkan/VkReconstruction.cpp
@@ -65,6 +65,12 @@ void VkReconstruction::save(android::base::Stream* stream) {
     dump();
 #endif
 
+    mApiTrace.forEachLiveEntry([](bool live, uint64_t handle, ApiInfo& info) {
+        if (live) {
+            info.saved = false;
+        }
+    });
+
     std::unordered_map<HandleWithState, int, HandleWithStateHash> totalParents;
     std::vector<HandleWithState> next;
 
@@ -104,8 +110,11 @@ void VkReconstruction::save(android::base::Stream* stream) {
         for (const auto& handle : handles) {
             auto item = mHandleReconstructions.get(handle.first)->states[handle.second];
             for (uint64_t apiRef : item.apiRefs) {
-#if DEBUG_RECONSTRUCTION
                 auto apiItem = mApiTrace.get(apiRef);
+                if (!apiItem) continue;
+                if (apiItem->saved) continue;
+                apiItem->saved = true;
+#if DEBUG_RECONSTRUCTION
                 DEBUG_RECON("adding handle 0x%lx API 0x%lx op code %d\n", handle.first, apiRef,
                             apiItem->opCode);
 #endif
@@ -267,10 +276,28 @@ VkReconstruction::ApiHandle VkReconstruction::createApiInfo() {
     return handle;
 }
 
+void VkReconstruction::removeHandleFromApiInfo(VkReconstruction::ApiHandle h, uint64_t toRemove) {
+    auto vk_item = mHandleReconstructions.get(toRemove);
+    if (!vk_item) return;
+    auto apiInfo = mApiTrace.get(h);
+    if (!apiInfo) return;
+
+    auto& handles = apiInfo->createdHandles;
+    auto it = std::find(handles.begin(), handles.end(), toRemove);
+
+    if (it != handles.end()) {
+        handles.erase(it);
+    }
+    DEBUG_RECON("removed 1 vk handle  0x%llx from apiInfo  0x%llx, now it has %d left",
+                (unsigned long long)toRemove, (unsigned long long)h, (int)handles.size());
+}
+
 void VkReconstruction::destroyApiInfo(VkReconstruction::ApiHandle h) {
     auto item = mApiTrace.get(h);
 
     if (!item) return;
+
+    if (!item->createdHandles.empty()) return;
 
     item->traceBytes = 0;
     item->createdHandles.clear();
@@ -428,6 +455,7 @@ void VkReconstruction::forEachHandleDeleteApi(const uint64_t* toProcess, uint32_
 
         for (auto& state : item->states) {
             for (auto handle : state.apiRefs) {
+                removeHandleFromApiInfo(handle, toProcess[i]);
                 destroyApiInfo(handle);
             }
             state.apiRefs.clear();
