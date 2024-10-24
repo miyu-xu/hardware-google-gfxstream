4fb40a9dd8ec306fd5af5b7d1b11aabd8a1fbce4
Serdar Kocdemir <kocdemir@google.com>
2024-10-24 11:12:58 +0000

    Replace stderr logging with appropriate logger routines
    Usages of fprintf(stderr,..) causes various issues when dealing with
    bugs. Replacing them with appropriate versions to ensure they are
    collected with any extra information needed and to avoid AS thinking
    there were a crash.
    
    Bug: 374311144
    Test: build and presubmit
    Change-Id: I6893a8df753ddfcc27f28dfd2bd828e53f3f32b3

--- host/PostWorker.cpp
+++ host/PostWorker.cpp
@@ -30,17 +30,15 @@
 
 #define POST_DEBUG 0
 #if POST_DEBUG >= 1
-#define DD(fmt, ...) \
-    fprintf(stderr, "%s:%d| " fmt, __func__, __LINE__, ##__VA_ARGS__)
+#define DD(fmt, ...) WARN(fmt, ##__VA_ARGS__)
 #else
 #define DD(fmt, ...) (void)0
 #endif
 
 #define POST_ERROR(fmt, ...)                                                  \
     do {                                                                      \
-        fprintf(stderr, "%s(%s:%d): " fmt "\n", __func__, __FILE__, __LINE__, \
+        ERR("%s(%s:%d): " fmt, __func__, __FILE__, __LINE__, \
                 ##__VA_ARGS__);                                               \
-        fflush(stderr);                                                       \
     } while (0)
 
 static void sDefaultRunOnUiThread(UiUpdateFunc f, void* data, bool wait) {
