3f985ea11ce80a12bf604ab7cbfcd673cde426af
Bo Hu <bohu@google.com>
2024-11-08 16:35:39 -0800

    wip: get snapshot work without batched descriptor
    This will make sure we property save and load DescriptorSet
    even when VulkanBatchedDescriptorSetUpdate feature is turned off.
    
    Bug: 370344466
    
    turn off deferredCommands, AsyncQueueSubmit
    
    Nov 6
    rebased after resolving merge conflict
    
    also avoid double box of command buffers
    fix a bug in forEachHandleAddModifyApi,
    where it always create a new modification,
    instead of checking existing one first.
    
    also clean modification for cmd buffer
    before vkBeginCommandBuffer
    
    also save vkUpdateDescriptorSets
    by introducint a new generic handle
    that just record this operation.
    this makes it easier to handle descriptor
    update, especially the dependency
    handling part.
    
    when there is nothing save, skip
    the rest of saving;
    
    when there is nothing loaded, skip
    the rest of loading.
    
    print the call stack when unbox
    fails
    
    export ANDROID_EMU_VK_LOG_CALLS=1
    export ANDROID_EMUGL_VERBOSE=1
    export ANDROID_EMU_VK_DISABLE_DEFERRED_COMMANDS=1
    
    for debugging using validation layer
    export VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_api_dump:VK_LAYER_KHRONOS_validation
    
    ./emulator -avd Pixel_8a_API_35_Play -feature VulkanSnapshots,-VulkanBatchedDescriptorSetUpdate,-VulkanQueueSubmitWithCommands,-QuickbootFileBacked $@
    
    TODO: cmd buffer depends on lots of resources,
    if any of those resources are destroyed, the cmdbuffer
    should be skipped during save; this will avoi cmdbuffer
    replay to crash during load, as the deps are gone.
    
    wip: modify command buffer handling
    
    problem: still cannot load to homescreen
    the vkCmdPipelineBarrier just crashes
    and the following change fixed it
    and load back chrome
    
    host/vulkan/VkDecoderSnapshot.cpp
    //class VkDecoderSnapshot::Impl {
    //         for (int i=0; i < bufferMemoryBarrierCount; ++i) {
    //             mReconstruction.addHandleDependency((const uint64_t*)&handle, 1,
    //                     (uint64_t)(uintptr_t)unboxed_to_boxed_non_dispatchable_VkBuffer(
    //-                        pBufferMemoryBarriers[i].buffer));
    //+                        pBufferMemoryBarriers[i].buffer), VkReconstruction::CREATED,
    //+                    VkReconstruction::BOUND_MEMORY);
    //         }
    
    problme: chrome locked up, because
    some apirefs does not have created
    any handles, so need to to add:
      OP_vkCmdBindIndexBuffer
      OP_vkCmdBlitImage
      OP_vkCmdClearAttachments
      OP_vkCmdCopyBufferToImage
      OP_vkCmdCopyImageToBuffer
      OP_vkCmdDrawIndexed
      OP_vkCmdPushConstants
      OP_vkCmdResolveImage
      OP_vkCmdSetScissor
      OP_vkCmdSetViewport
    
    so the steps are as follows,
    star from vkBeginCommandBuffer,
    create a generic handle fo each command,
    make vkBeginCommandBuffer depdends on <commandbuffer, CMD_RECORD>
    make vkCmdx depends on vkBeginCommandBuffer
    make vkCmdy depends on vkCmdx
    ..
    make vkEndCommandBuffer depends on last command
    
    note, each <commandbuffer, CMD_RECORD> should have
    at most one child.
    
    however, problems:
    the vkBeginCommandBuffer ... vkEndCommandBuffer
    should go as a set, and there should not be
    any middle command mixed. how to achieve that ?
    
    actually, this is not a problem, since each
    command has its first parameter of cmdBuffer,
    so they wont mix up.
    
    the ordering is already embedded in the dep
    graph, so no need to have the separate
    modify apis.
    
    disable feature deps check
    
    wip
    
    Change-Id: I5f2ac07b0ae1aed0a8caa869898eb873ce940418

--- host/vulkan/VkReconstruction.cpp
+++ host/vulkan/VkReconstruction.cpp
@@ -25,7 +25,7 @@
 namespace gfxstream {
 namespace vk {
 
-#define DEBUG_RECONSTRUCTION 0
+#define DEBUG_RECONSTRUCTION 1
 
 #if DEBUG_RECONSTRUCTION
 
@@ -58,7 +58,7 @@ std::vector<VkReconstruction::HandleWithState> typeTagSortedHandles(
     return res;
 }
 
-void VkReconstruction::save(android::base::Stream* stream) {
+int VkReconstruction::save(android::base::Stream* stream) {
     DEBUG_RECON("start")
 
 #if DEBUG_RECONSTRUCTION
@@ -111,8 +111,8 @@ void VkReconstruction::save(android::base::Stream* stream) {
                 if (savedApis.find(apiRef) != savedApis.end()) continue;
                 savedApis.insert(apiRef);
 #if DEBUG_RECONSTRUCTION
-                DEBUG_RECON("adding handle 0x%lx API 0x%lx op code %d", handle.first, apiRef,
-                            apiItem->opCode);
+                DEBUG_RECON("adding handle 0x%lx API 0x%lx op code %d name %s\n", handle.first, apiRef,
+                            apiItem->opCode, api_opcode_to_string(apiItem->opCode));
 #endif
                 nextApis.push_back(apiRef);
             }
@@ -156,7 +156,8 @@ void VkReconstruction::save(android::base::Stream* stream) {
         for (auto apiHandle : uniqApiRefsByTopoOrder[i]) {
             auto item = mApiTrace.get(apiHandle);
             // 4 bytes for opcode, and 4 bytes for saveBufferRaw's size field
-            DEBUG_RECON("saving api handle 0x%lx op code %d", apiHandle, item->opCode);
+            DEBUG_RECON("saving api handle 0x%lx op code %d name %s\n",
+                    apiHandle, item->opCode, api_opcode_to_string(item->opCode));
             memcpy(apiTracePtr, &item->opCode, sizeof(uint32_t));
             apiTracePtr += 4;
             uint32_t traceBytesForSnapshot = item->traceBytes + 8;
@@ -175,6 +176,8 @@ void VkReconstruction::save(android::base::Stream* stream) {
     android::base::saveBufferRaw(stream, (char*)(createdHandleBuffer.data()),
                                  createdHandleBuffer.size() * sizeof(uint64_t));
     android::base::saveBufferRaw(stream, (char*)(apiTraceBuffer.data()), apiTraceBuffer.size());
+
+    return (int)( createdHandleBuffer.size() +  apiTraceBuffer.size());
 }
 
 class TrivialStream : public IOStream {
@@ -220,7 +223,7 @@ class TrivialStream : public IOStream {
     virtual unsigned char* onLoad(android::base::Stream* stream) { return nullptr; }
 };
 
-void VkReconstruction::load(android::base::Stream* stream, emugl::GfxApiLogger& gfxLogger,
+int VkReconstruction::load(android::base::Stream* stream, emugl::GfxApiLogger& gfxLogger,
                             emugl::HealthMonitor<>* healthMonitor) {
     DEBUG_RECON("start. assuming VkDecoderGlobalState has been cleared for loading already");
     mApiTrace.clear();
@@ -234,6 +237,10 @@ void VkReconstruction::load(android::base::Stream* stream, emugl::GfxApiLogger& 
 
     DEBUG_RECON("created handle buffer size: %zu trace: %zu", createdHandleBuffer.size(),
                 apiTraceBuffer.size());
+    const int totalBytes = (createdHandleBuffer.size() + apiTraceBuffer.size());
+    if (totalBytes == 0) {
+        return 0;
+    }
 
     uint32_t createdHandleBufferSize = createdHandleBuffer.size();
 
@@ -265,6 +272,7 @@ void VkReconstruction::load(android::base::Stream* stream, emugl::GfxApiLogger& 
                              context);
 
     DEBUG_RECON("finished decoding trace");
+    return totalBytes;
 }
 
 VkReconstruction::ApiHandle VkReconstruction::createApiInfo() {
@@ -483,6 +491,7 @@ void VkReconstruction::addHandleDependency(const uint64_t* handles, uint32_t cou
     for (uint32_t i = 0; i < count; ++i) {
         auto childItem = mHandleReconstructions.get(handles[i]);
         if (!childItem) {
+            DEBUG_RECON("WARN: adding null child item: 0x%lx\n", handles[i]);
             continue;
         }
         parentItemState.childHandles.insert({handles[i], static_cast<HandleState>(childState)});
@@ -522,9 +531,21 @@ void VkReconstruction::forEachHandleAddModifyApi(const uint64_t* toProcess, uint
             item = mHandleModifications.get(toProcess[i]);
         }
 
-        if (!item) continue;
+        DEBUG_RECON("add modify api for handle 0x%lx with apiHandle 0x%lx",
+                toProcess[i], apiHandle);
+
+        if (!item) {
+        DEBUG_RECON("BUT cannot find item for handle 0x%lx", toProcess[i]);
+            continue;
+        }
 
         item->apiRefs.push_back(apiHandle);
+        DEBUG_RECON("Now there are total of api ref %d", (int)(item->apiRefs.size()));
+        int j=0;
+        for (auto ref: item->apiRefs) {
+            DEBUG_RECON("api ref %d: 0x%lx", j, ref);
+            ++j;
+        }
     }
 }
 
@@ -540,7 +561,27 @@ void VkReconstruction::forEachHandleClearModifyApi(const uint64_t* toProcess, ui
     }
 }
 
-std::vector<uint64_t> VkReconstruction::getOrderedUniqueModifyApis() const {
+uint64_t VkReconstruction::getLastModifyApiOpHandle(uint64_t commandBuffer) {
+    if (!commandBuffer) return 0;
+
+    auto item = mHandleModifications.get(commandBuffer);
+    if (!item) return 0;
+
+    if (item->apiRefs.empty()) return 0;
+
+    auto last = item->apiRefs.size() - 1;
+    auto apiHandle = item->apiRefs[last];
+    auto itemApi = mApiTrace.get(apiHandle);
+    if (itemApi && itemApi->createdHandles.size() > 0) {
+        return itemApi->createdHandles[0];
+    }
+    fprintf(stderr, "%s %s %d failed: the api 0x%llx name %s does no create any handles\n",
+            __FILE__, __func__, __LINE__, (unsigned long long)apiHandle,
+            itemApi ? api_opcode_to_string(itemApi->opCode) : "unknown");
+    return 0;
+}
+
+std::vector<uint64_t> VkReconstruction::getOrderedUniqueModifyApis() {
     std::vector<HandleModification> orderedModifies;
 
     // Now add all handle modifications to the trace, ordered by the .order field.
@@ -559,9 +600,20 @@ std::vector<uint64_t> VkReconstruction::getOrderedUniqueModifyApis() const {
 
     std::unordered_set<uint64_t> usedModifyApis;
     std::vector<uint64_t> orderedUniqueModifyApis;
+    for (const auto apiHandle:usedModifyApis) {
+            auto apiInfo = mApiTrace.get(apiHandle);
+            const char* apiName = apiInfo ? api_opcode_to_string(apiInfo->opCode) : "unalloced";
+            fprintf(stderr, "VkReconstruction::%s: mod:     0x%llx: %s\n", __func__,
+                    (unsigned long long)apiHandle, apiName);
+    }
 
+    fprintf(stderr, "before uniq apiRef\n");
     for (const auto& mod : orderedModifies) {
         for (auto apiRef : mod.apiRefs) {
+            auto apiInfo = mApiTrace.get(apiRef);
+            const char* apiName = apiInfo ? api_opcode_to_string(apiInfo->opCode) : "unalloced";
+            fprintf(stderr, "VkReconstruction::%s: mod:     0x%llx: %s\n", __func__,
+                    (unsigned long long)apiRef, apiName);
             if (usedModifyApis.find(apiRef) == usedModifyApis.end()) {
                 orderedUniqueModifyApis.push_back(apiRef);
                 usedModifyApis.insert(apiRef);
@@ -569,6 +621,13 @@ std::vector<uint64_t> VkReconstruction::getOrderedUniqueModifyApis() const {
         }
     }
 
+    fprintf(stderr, "after uniq apiRef\n");
+    for (const auto apiHandle:orderedUniqueModifyApis) {
+            auto apiInfo = mApiTrace.get(apiHandle);
+            const char* apiName = apiInfo ? api_opcode_to_string(apiInfo->opCode) : "unalloced";
+            fprintf(stderr, "VkReconstruction::%s: mod:     0x%llx: %s\n", __func__,
+                    (unsigned long long)apiHandle, apiName);
+    }
     return orderedUniqueModifyApis;
 }
 
