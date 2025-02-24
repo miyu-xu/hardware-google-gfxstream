24af45f0a0420a81d10a2165ecb97f59550187ec
Jason Macnak <natsu@google.com>
2025-02-24 13:35:39 -0800

    Replace boxed handle preprocessor usage with template functions
    ... as dealing with the preprocessor is a PITA when trying to add
    some simple code for debugging. The preprocessor usage also does
    not play nicely with references in code search.
    
    Bug: b/369615058
    Test: cvd create --gpu_mode=gfxstream_guest_angle_host_swiftshader
    Change-Id: I295a66de3545a5f5a53697111c613b4844f96a0c

--- host/vulkan/VulkanBoxedHandles.h
+++ host/vulkan/VulkanBoxedHandles.h
@@ -208,6 +208,7 @@ extern BoxedHandleManager sBoxedHandleManager;
 #define DEFINE_BOXED_NON_DISPATCHABLE_HANDLE_API_DECL(type)                             \
     type new_boxed_non_dispatchable_##type(type underlying);                            \
     void delete_##type(type boxed);                                                     \
+    void delayed_delete_##type(type boxed, VkDevice device, std::function<void()> callback); \
     type unbox_##type(type boxed);                                                      \
     type try_unbox_##type(type boxed);                                                  \
     type unboxed_to_boxed_non_dispatchable_##type(type boxed);                          \
