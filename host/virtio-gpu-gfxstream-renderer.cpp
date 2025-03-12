aa60b414a4a8eaee90a9d2bd34d53d7617ea60df
Gurchetan Singh <gurchetansingh@chromium.org>
2025-03-12 13:38:28 -0800

    gfxstream: fix segfault with meson build
    Segmentation fault here:
    
    Program received signal SIGSEGV, Segmentation fault.
    0x00007ffff79cd693 in getGraphicsAgents () at /home/lenovo/aemu/host-common/GraphicsAgentFactory.cpp:36
    36              *(uint32_t*)(1234) = 5;
    (gdb) bt
    0  0x00007ffff79cd693 in getGraphicsAgents () at /home/lenovo/aemu/host-common/GraphicsAgentFactory.cpp:36
    1  0x00007ffff744a5c4 in stream_renderer_opengles_init (display_width=1280, display_height=1024, renderer_flags=-2147483551, features=...)
    
    BUG=317816825
    TEST=CI
    
    Change-Id: Id3fc9ffac1f9a776ff38847d9963350d5cee6409

--- host/virtio-gpu-gfxstream-renderer.cpp
+++ host/virtio-gpu-gfxstream-renderer.cpp
@@ -608,8 +608,7 @@ static int stream_renderer_opengles_init(uint32_t display_width, uint32_t displa
 
     int maj;
     int min;
-    android_startOpenglesRenderer(display_width, display_height, 1, 28, getGraphicsAgents()->vm,
-                                  getGraphicsAgents()->emu, getGraphicsAgents()->multi_display,
+    android_startOpenglesRenderer(display_width, display_height, 1, 28, nullptr, nullptr, nullptr,
                                   &features, &maj, &min);
 
     char* vendor = nullptr;
@@ -627,7 +626,6 @@ static int stream_renderer_opengles_init(uint32_t display_width, uint32_t displa
         return -EINVAL;
     }
 
-    address_space_set_vm_operations(getGraphicsAgents()->vm);
     android_init_opengles_pipe();
     android_opengles_pipe_set_recv_mode(2 /* virtio-gpu */);
     android_init_refcount_pipe();
